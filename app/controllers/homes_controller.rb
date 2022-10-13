be2ae2dd73379f80d0aa4737964a4d68ae667c1a
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-10-13 16:45:35 +0530

    feat: drag-drop update for developer status

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,14 @@
 class HomesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
-    @kanban_columns  = KanbanColumn.includes(:client_requests,:departments,:developers).all
-    @client_requests  = ClientRequest.includes(:departments,:developers).all
-  end  
+    @kanban_columns = KanbanColumn.includes(:client_requests, :departments, :developers).all
+    @client_requests = ClientRequest.includes(:departments, :developers).all
+  end
+
+  def developer_status_update
+    k = KanbanColumn.find_by(id: params['kanban_col_id'])
+    d = Developer.find_by(id: params['developer_id'])
+    d.update!(developer_status: k.name)
+    binding.pry
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "homes#index"
+  root 'homes#index'
+  post 'developer_status_update', to: 'homes#developer_status_update'
 end
